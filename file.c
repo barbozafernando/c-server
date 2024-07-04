98715a358f779c51520c6da5f9f6f8596bea0169
Fernando Barboza <fernando.barboza@gazin.com.br>
2024-07-04 20:14:51 -0300

    feat: case any specific file was requested return the default one

--- file.c
+++ file.c
@@ -1,18 +1,29 @@
+#include <fcntl.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #include "file.h"
 
-const char *get_filename_from_request(char *buf) {
-  char *f = buf + 5;
-  *strchr(f, ' ') = 0;
+const char *get_filename_from_request(char *request) {
+  const char *DEFAULT_FILENAME = "index.html";
+  const char *filename = {0};
+  size_t offset = 5;
 
-  return f;
+  char *s = request + offset;
+
+  if (s[0] == 32) {
+    return DEFAULT_FILENAME;
+  }
+
+  size_t nbytes = strcspn(s, " ");
+  strncpy((char *)filename, request, nbytes);
+
+  return filename;
 }
 
 char *read_file(const char *filename) {
@@ -27,7 +38,7 @@ char *read_file(const char *filename) {
   if (fd == -1) {
     perror("file not found");
   }
-  
+
   int bytes_read = read(fd, ptr_buf, FILE_MAX_SIZE);
 
   if (bytes_read < 0) {
