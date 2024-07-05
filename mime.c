4ed31b8258bb07badd80e6b4749ea886c2b5bc82
barbozafernando <fernando.barboza@gazin.com.br>
2024-07-05 08:04:35 -0300

    fix: remove strlower call for now

--- mime.c
+++ mime.c
@@ -5,7 +5,7 @@
 
 char *strlower(char *s) {
   for (char *p = s; *p != '\0'; p++) {
-      *p = tolower(*p);
+    *p = tolower(*p);
   }
 
   return s;
@@ -15,23 +15,37 @@ void get_mime_type(const char *filename, char buf[]) {
   char *ext = strrchr(filename, '.');
 
   if (ext == NULL) {
-    strcpy(buf,DEFAULT_MIME_TYPE);
+    strcpy(buf, DEFAULT_MIME_TYPE);
     return;
   }
-  
+
   ext++;
 
-  strlower(ext);
-
-  if (strcmp(ext, "html") == 0 || strcmp(ext, "htm") == 0) { strcpy(buf, "text/html"); }
-  if (strcmp(ext, "jpeg") == 0 || strcmp(ext, "jpg") == 0) { strcpy(buf, "image/jpg"); }
-  if (strcmp(ext, "css") == 0) { strcpy(buf, "text/css"); }
-  if (strcmp(ext, "js") == 0) { strcpy(buf, "application/javascript"); }
-  if (strcmp(ext, "json") == 0) { strcpy(buf, "application/json"); }
-  if (strcmp(ext, "txt") == 0) { strcpy(buf, "text/plain"); }
-  if (strcmp(ext, "gif") == 0) { strcpy(buf, "image/gif"); }
-  if (strcmp(ext, "png") == 0) { strcpy(buf, "image/png"); }
+  if (strcmp(ext, "html") == 0 || strcmp(ext, "htm") == 0) {
+    strcpy(buf, "text/html");
+  }
+  if (strcmp(ext, "jpeg") == 0 || strcmp(ext, "jpg") == 0) {
+    strcpy(buf, "image/jpg");
+  }
+  if (strcmp(ext, "css") == 0) {
+    strcpy(buf, "text/css");
+  }
+  if (strcmp(ext, "js") == 0) {
+    strcpy(buf, "application/javascript");
+  }
+  if (strcmp(ext, "json") == 0) {
+    strcpy(buf, "application/json");
+  }
+  if (strcmp(ext, "txt") == 0) {
+    strcpy(buf, "text/plain");
+  }
+  if (strcmp(ext, "gif") == 0) {
+    strcpy(buf, "image/gif");
+  }
+  if (strcmp(ext, "png") == 0) {
+    strcpy(buf, "image/png");
+  }
 
   if (buf == NULL)
-    strcpy(buf,DEFAULT_MIME_TYPE);
+    strcpy(buf, DEFAULT_MIME_TYPE);
 }
